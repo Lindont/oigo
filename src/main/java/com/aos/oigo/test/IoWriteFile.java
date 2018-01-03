2788fc62a3b7d165a68b3869d12ce1a809620e9b
Lindont <coovoo@vip.qq.com>
2018-01-03 17:54:24 +0800

    delect file

--- src/main/java/com/aos/oigo/test/IoWriteFile.java
+++ src/main/java/com/aos/oigo/test/IoWriteFile.java
@@ -1,22 +1,24 @@
 package com.aos.oigo.test;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 /**
  * @Author : HuangHaoXin
- * @Description :
+ * @Description : 递归遍历某目录下所有文件，文件夹，子文件和子文件夹，重命名文件，并删除指定文件
  * @Date : Create in 2018-01-03
  */
 public class IoWriteFile {
-    //遍历某目录下所有文件，文件夹，子文件和子文件夹
+    Logger logger = LoggerFactory.getLogger(IoWriteFile.class);
     public void traverseFolder(String path) {
         File file = new File(path);
         if (file.exists()) {
             File[] files = file.listFiles();
             if (files.length == 0) {
-                System.out.println("------文件夹是空的------");
+                System.out.println("文件夹是空的");
                 return;
             } else {
                 for (File f : files) {
@@ -31,16 +33,25 @@ public class IoWriteFile {
                         if (f.getAbsolutePath().indexOf("[www.17zixueba.com]") != -1) {
                             f.renameTo(new File(f.getAbsolutePath().replace("[www.17zixueba.com]", "")));
                         }
+
+                        if ("一起自学吧 - IT视频教程分享社区.url".equals(f.getName()) ||
+                                "学习路线.png".equals(f.getName()) ||
+                                "课前必读.txt".equals(f.getName())) {
+                            logger.info(f.getName());
+                            f.delete();
+                        }
                     }
                 }
             }
         } else {
-            System.out.println("------文件不存在------");
+            System.out.println("文件不存在");
         }
     }
 
+
+
     @Test
     public void test() {
-        traverseFolder("G:\\学习资料");
+        traverseFolder("F:\\BaiduNetdiskDownload");
     }
 }
