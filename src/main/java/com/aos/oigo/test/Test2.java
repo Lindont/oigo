163f131f2dc997318c9b8ab9c4529c43c4265a44
Lindont <coovoo@vip.qq.com>
2018-02-09 17:13:08 +0800

    commit 29

--- src/main/java/com/aos/oigo/test/Test2.java
+++ src/main/java/com/aos/oigo/test/Test2.java
@@ -1,7 +1,9 @@
 package com.aos.oigo.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,43 +12,12 @@ import java.util.List;
  * @Description :
  * @Date : Create in 2017-11-10
  */
+@Slf4j
 public class Test2 {
     @Test
     public void test1() {
         int count = 3;
-        for (int i = 1; i<=count; i++) {
-            if (i == 2) {
-                break;
-            }
-            System.out.println(i);
-        }
-        System.out.println("asd");
-    }
-
-
-    @Test
-    public void test2() {
-        List list = new ArrayList();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
-        list.add("e");
-        list.add("f");
-
-
-//        int size = 0;
-//        for (int i = 0;i<list.size();i++) {
-//            size++;
-//        }
-//        System.out.println(size);
-        for (int i=0;i<list.size();i++) {
-            if (i+1<list.size()) {
-                if ( i == 2) {
-
-                }
-                System.out.println(list.get(i)+" + "+list.get(i+1));
-            }
-        }
+        System.out.println(MessageFormat.format("count:{0}", count));
+        log.info("count: {}, message: {}", count, count + 2);
     }
 }
